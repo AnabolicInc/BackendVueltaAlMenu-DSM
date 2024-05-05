e13c39c5c0141e35f4d2f3e52ac077f97c154dbc
Royman Valencia <royman.valencia@alumnos.ucn.cl>
2024-05-05 19:38:25 -0400

    Avance CRUD Category

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -2,6 +2,7 @@ const { Router, request, response } = require("express");
 const { check } = require("express-validator");
 const { validateFields } = require("../middlewares/validate-fields");
 const { createCategory, updateCategory } = require("../controllers/categoryController");
+const { route } = require("./authRoutes");
 
 
 const router = Router(); 
@@ -9,6 +10,7 @@ const router = Router();
 router.post('/createCategory',[
     check('name', 'the field name is required').not().isEmpty(),
     check('name', 'the field name is required').isString(),
+
     validateFields
 ], createCategory);
 
@@ -19,5 +21,13 @@ router.post('/updateCategory',[
     check('name', 'the field name is required').isString(),
     validateFields
 ], updateCategory);
+
+router.get('/getCategories'[
+    validateFields
+], getCategories);
+
+router.delete('/deleteCategory/:id',[
+    validateFields
+], deleteCategory);
 
 module.exports = router;
