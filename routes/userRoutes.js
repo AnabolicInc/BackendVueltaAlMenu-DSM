4b13dce381f9265b6a871d1a046a2941796498c4
tortaaaa <pablo.reyes01@alumnos.ucn.cl>
2024-05-16 21:34:51 -0400

    feat: route for changepassword requirement added

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,9 @@
 const { Router } = require("express");
 const { check } = require("express-validator");
-const { getUsers, changePassword, putUser } = require("../controllers/usersController");
+const { getUsers, changePassword, putUser, newPassword } = require("../controllers/usersController");
 const { validateJWT } = require("../middlewares/validate-jwt");
 const { validateFields } = require("../middlewares/validate-fields");
 const { verifyEmailLogin } = require("../helpers/verify-email");
-
 
 const router = Router();
 
@@ -18,15 +17,20 @@ router.put('/:id', [
     check('lastName', 'The lastName is required').not().isEmpty(),
     check('phone', 'The phone is required').not().isEmpty(),
     validateFields
-
 ], putUser);
 
-router.post('/change-password',[
+router.post('/change-password', [
     check('email', 'the field email is required').not().isEmpty(),
     check('email', 'this not valid email').isEmail(),
     check('email', 'the field email is required').custom(verifyEmailLogin),
     validateFields
 ], changePassword);
 
+router.put('/new-password', [
+    validateJWT,
+    check('email', 'the field email is required').not().isEmpty(),
+    check('newPassword', 'The new password is required').not().isEmpty(),
+    validateFields
+], newPassword);
 
 module.exports = router;
