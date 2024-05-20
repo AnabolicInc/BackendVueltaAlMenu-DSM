467442471c93ac29de5ff787ebe2e1ccac719792
Giantt2 <giantt67@gmail.com>
2024-05-20 19:30:44 -0400

    feat(CreateProduct): PRO-001 implemented

---
+++ routes/productRoutes.js
@@ -0,0 +1,39 @@
+const { check } = require("express-validator");
+const { Router, request, response } = require("express");
+const { validateFields } = require("../middlewares/validate-fields");
+const { createProduct, updateProduct, listProducts, deleteProduct } = require("../controllers/productController");
+
+
+const router = Router(); 
+
+router.post('/createProduct/:category_id',[
+    check('name', 'the field name is required').not().isEmpty(),
+    check('name', 'the field name is required').isString(),
+    check('price', 'the field description is required').not().isEmpty(),
+    check('price', 'the field description is required').isNumeric(),
+    check('description', 'the field description is required').not().isEmpty(),
+    check('description', 'the field description is required').isString(),
+    check('quantity', 'the field description is required').not().isEmpty(),
+    check('quantity', 'the field description is required').isNumeric(),
+    validateFields
+], createProduct);
+
+
+
+router.put('/updateProduct/:id',[
+    check('name', 'the field name is required').not().isEmpty(),
+    check('name', 'the field name is required').isString(),
+    check('description', 'the field description is required').not().isEmpty(),
+    check('description', 'the field description is required').isString(),
+    validateFields
+], updateProduct);
+
+router.get('/listProducts', [
+    
+], listProducts);
+
+router.delete('/deleteProduct/:id',[
+    validateFields
+], deleteProduct);
+
+module.exports = router;
