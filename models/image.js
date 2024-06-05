46e064d3aae3f07d9dcdfce3a75b26790ad51f07
tortaaaa <pablo.reyes01@alumnos.ucn.cl>
2024-06-05 07:25:37 -0400

    refactor: bad name on modelName corrected and auto increment assigned to id

--- models/image.js
+++ models/image.js
@@ -1,22 +1,25 @@
 const { Model, DataTypes } = require("sequelize");
 const db = require("../db/connection");
 
-class Image extends Model {
-    static id;
-    static uri;
-    static product_id;
-}
+class Image extends Model {}
 
 Image.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
     uri: {
         type: DataTypes.STRING,
+        allowNull: false
     },
     product_id: {
         type: DataTypes.INTEGER,
-    },
+        allowNull: false
+    }
 }, {
-    sequelize : db,
-    modelName: 'Image,'
+    sequelize: db,
+    modelName: 'Image',
 });
 
-module.exports=Image;
+module.exports = Image;
