01bfab9baa0b99f616115c9465b6e6d94e01bde4
Royman Valencia <royman.valencia@alumnos.ucn.cl>
2024-06-07 17:18:50 -0400

    :fix(productList): list only active products

--- controllers/productController.js
+++ controllers/productController.js
@@ -5,11 +5,12 @@ const { check } = require("express-validator");
 const bcryptjs = require("bcryptjs");
 const generateJWT = require("../helpers/generate-jwt");
 const jwt = require("jsonwebtoken");
+const { where } = require("sequelize");
 
 
 const listProducts = async (req = request, res = response) => {
     try {
-        const products = await Product.findAll();
+        const products = await Product.findAll({where: {status: 1}});
 
         res.status(200).json({
             success: true,
@@ -25,34 +26,6 @@ const listProducts = async (req = request, res = response) => {
     }
 }
 
-const deleteCategory = async (req = request, res = response) => {
-    try {
-        const { id } = req.params;
-        
-        const category = await Category.findByPk(id);
-
-        if (!category) {
-            return res.status(400).json({
-                success: false,
-                message: `Category not exist, ID: ${id}`
-            });
-        }
-
-        await category.update({ status: 0 });
-
-        res.status(201).json({
-            success: true,
-            message: 'Category deleted'
-        });
-
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            success: false,
-            message: 'Server error'
-        });
-    }
-}
 const createProduct = async (req = request, res = response) => {
     try {
 
